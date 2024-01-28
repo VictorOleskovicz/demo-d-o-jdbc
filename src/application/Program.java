a045442b41018a7f29b387f383b957dbde298da7
Victor Oleskovicz <victoroleskovicz1@gmail.com>
2024-01-27 21:27:43 -0300

    Updating

--- src/application/Program.java
+++ src/application/Program.java
@@ -29,15 +29,20 @@ public class Program {
 		System.out.println("==== 3rd test, find all ==== ");
 		list = sellerDao.findAll();
 		for (Seller sellers : list) {
-			System.out.println(sellers);
+			System.out.println(sellers + "\n");
 		}
 		
 		System.out.println("==== 4ft test, insert ==== ");
 
 		Seller seller1 = new Seller(null, "Mario", "mario@gmail.com", new Date(), 4000.0, dep);
 		sellerDao.insert(seller1);
-		System.out.println("Inserted" + seller1.getId());
+		System.out.println("Inserted" + seller1.getId() +  "\n");
 		
+		System.out.println("==== 5ft test, update ==== ");
+		Seller seller2 = sellerDao.findById(1);
+		seller2.setName("Maria Bulha");
+		sellerDao.update(seller2);
+		System.out.println("Done!");
 		
 	}
 
